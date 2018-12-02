453d3fe484a53df6cb5a3aed77f8e2c6ae694377
Nadin-Katrin Apel <nadin-katrin.apel@porsche.de>
2018-12-02 17:36:23 +0100

    pattern-lab-test

---
+++ 05_pattern_lab/pattern-lab/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <v-toolbar>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title>Title</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>Link One</v-btn>
+        <v-btn flat>Link Two</v-btn>
+        <v-btn flat>Link Three</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <img alt="Vue logo" src="./assets/logo.png">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 05_pattern_lab/pattern-lab/src/main.js
@@ -0,0 +1,45 @@
+require('../node_modules/vuetify/src/stylus/app.styl')
+
+import Vue from 'vue'
+import {
+    Vuetify,
+    VApp,
+    VNavigationDrawer,
+    VFooter,
+    VList,
+    VBtn,
+    VIcon,
+    VGrid,
+    VToolbar,
+    transitions
+} from 'vuetify'
+import App from './App.vue'
+
+Vue.use(Vuetify, {
+    components: {
+        VApp,
+        VNavigationDrawer,
+        VFooter,
+        VList,
+        VBtn,
+        VIcon,
+        VGrid,
+        VToolbar,
+        transitions
+    },
+    theme: {
+        primary: '#ee44aa',
+        secondary: '#424242',
+        accent: '#82B1FF',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107'
+    }
+})
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
