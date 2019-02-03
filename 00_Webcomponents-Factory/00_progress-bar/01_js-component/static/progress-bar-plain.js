8aeb10e7cc9ffa9dd37132ec0f517ae4c9dac872
Nadin-Katrin Apel <na031@hdm-stuttgart.de>
2019-02-03 11:23:41 +0100

    code cleanup

--- 00_Webcomponents-Factory/00_progress-bar/01_js-component/static/progress-bar-plain.js
+++ 00_Webcomponents-Factory/00_progress-bar/01_js-component/static/progress-bar-plain.js
@@ -1,19 +1,11 @@
-let renderArr = []
-let renderArrComponent = []
-let renderArrSetter = []
-
 class ProgressBar {
     constructor(parentNode, complete) {
-        // const start=window.performance.now()
-
         this._complete = complete || 0;
         this._parentNode = parentNode;
         this._bar = document.createElement('div')
         this._barInner = document.createElement('div');
 
         this.renderTemplate();
-
-        // renderArrComponent.push(window.performance.now()-start)
     }
 
     get complete() {
@@ -21,18 +13,13 @@ class ProgressBar {
     }
 
     set complete(val) {
-        // const start=window.performance.now()
-
         this._complete = val;
         this._barInner.style.width = this._complete + '%';
         this._barInner.textContent = this._complete + '%';
 
-        // renderArrSetter.push(window.performance.now()-start)
     }
 
     renderTemplate() {
-        // const start=window.performance.now()
-
         this._bar.classList.add('progress-bar');
         this._barInner.classList.add('progress-bar-inner');
         this._barInner.textContent = this._complete + '%';
@@ -56,9 +43,9 @@ class ProgressBar {
                             linear-gradient(127deg, rgba(156,17,255,0.8), rgba(255,0,0,0) 70.71%),
                             linear-gradient(336deg, rgba(0,0,255,.8), rgba(0,0,255,0) 70.71%);
                             background-size: 400% 400%;
-                -webkit-animation: GradientProgressBar-${this._id} 1.6s ease infinite;
-	            -moz-animation: GradientProgressBar-${this._id} 2s ease infinite;
-                animation: GradientProgressBar-${this._id} 1.9s ease infinite;
+                -webkit-animation: GradientProgressBar 1.6s ease infinite;
+	            -moz-animation: GradientProgressBar 2s ease infinite;
+                animation: GradientProgressBar 1.9s ease infinite;
                 animation-iteration-count: 4;
                 text-align: center;
                 border-radius: 5px;
@@ -71,7 +58,7 @@ class ProgressBar {
 
         // append global styling, keyframes cant be added to element
         ProgressBar.insertStyleSheetRule(
-            `@keyframes GradientProgressBar-${this._id} {
+            `@keyframes GradientProgressBar {
                     0% {
                         background-position: 0% 50%
                      }
@@ -83,7 +70,6 @@ class ProgressBar {
                      }
              }`
         )
-       // renderArr.push(window.performance.now()-start)
     }
 
     static insertStyleSheetRule(ruleText) {
@@ -98,70 +84,4 @@ class ProgressBar {
         let sheet = sheets[sheets.length - 1];
         sheet.insertRule(ruleText, sheet.rules ? sheet.rules.length : sheet.cssRules.length)
     }
- /*   static get performance () {
-        let p = {
-            renderTemplate: {
-                '0_array': renderArr,
-                '1_sort': renderArr.sort(),
-                '2_quantile': {
-                    l:renderArr.sort()[Math.round((renderArr.length - 1)*0.25)].toFixed(3),
-                    r:renderArr.sort()[Math.round((renderArr.length - 1)*0.75)+1].toFixed(3),
-                },
-                '4_min': Math.min(...renderArr).toFixed(3),
-                '5_max': Math.max(...renderArr).toFixed(3),
-                '6_mean': (renderArr.reduce((a,b) => a+b)/(renderArr.length-1)).toFixed(3),
-                '7_sum': renderArr.reduce((a,b) => a+b).toFixed(3),
-            },
-            renderComponent: {
-                '0_array': renderArrComponent,
-                '1_sort': renderArrComponent.sort(),
-                '2_quantile': {
-                    l:renderArrComponent.sort()[Math.round((renderArrComponent.length - 1)*0.25)].toFixed(3),
-                    r:renderArrComponent.sort()[Math.round((renderArrComponent.length - 1)*0.75)+1].toFixed(3),
-                },
-                '4_min': Math.min(...renderArrComponent).toFixed(3),
-                '5_max': Math.max(...renderArrComponent).toFixed(3),
-                '6_mean': (renderArrComponent.reduce((a,b) => a+b)/(renderArrComponent.length-1)).toFixed(3),
-                '7_sum': renderArrComponent.reduce((a,b) => a+b).toFixed(3),
-            },
-            setWidth: renderArrSetter.length > 0 ? {
-                '0_array': renderArrSetter,
-                '1_sort': renderArrSetter.sort(),
-                '2_quantile': {
-                    l:renderArrSetter.sort()[Math.round((renderArrSetter.length - 1)*0.25)].toFixed(3),
-                    r:renderArrSetter.sort()[Math.round((renderArrSetter.length - 1)*0.75)+1].toFixed(3),
-                },
-                '4_min': Math.min(...renderArrSetter).toFixed(3),
-                '5_max': Math.max(...renderArrSetter).toFixed(3),
-                '6_mean': (renderArrSetter.reduce((a,b) => a+b)/(renderArrSetter.length-1)).toFixed(3),
-                '7_sum': renderArrSetter.reduce((a,b) => a+b).toFixed(3),
-            }: {},
-        }
-        return {
-            component: {
-                max: p.renderComponent["5_max"],
-                min: p.renderComponent["4_min"],
-                mean: p.renderComponent["6_mean"],
-                sum: p.renderComponent["7_sum"],
-                ql: p.renderComponent["2_quantile"][0],
-                qr: p.renderComponent["2_quantile"][1],
-            },
-            template: {
-                max: p.renderTemplate["5_max"],
-                min: p.renderTemplate["4_min"],
-                mean: p.renderTemplate["6_mean"],
-                sum: p.renderTemplate["7_sum"],
-                ql: p.renderTemplate["2_quantile"][0],
-                qr: p.renderTemplate["2_quantile"][1],
-            },
-            attrChange: {
-                max: p.setWidth["5_max"],
-                min: p.setWidth["4_min"],
-                mean: p.setWidth["6_mean"],
-                sum: p.setWidth["7_sum"],
-                ql: p.setWidth["2_quantile"][0],
-                qr: p.setWidth["2_quantile"][1],
-            }
-        }
-    }*/
 }
